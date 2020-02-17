e4e0b663a601a12628fa5becdcf564460dbff290
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-02-17 15:57:08 -0300

    Manipulando componentes com classe.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 
-import link from './assets/link-png-7.png'
+import TechList from './components/TechList';
 
 function App() {
-  return <img width="200" src={link} />
+  return <TechList />
 }
 
 export default App;  
